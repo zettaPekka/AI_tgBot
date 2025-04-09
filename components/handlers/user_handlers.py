5d321816c79e9f20157e985f6d2962a0c1e97b67
David <kigeandaviddd@gmail.com>
2025-04-09 23:05:41 +0300

    commit

--- components/handlers/user_handlers.py
+++ components/handlers/user_handlers.py
@@ -56,6 +56,12 @@ async def chat_active(message: Message, state: FSMContext):
             ai_response = await answer_to_text_prompt(main_prompt=message.text, tg_id=message.from_user.id)
             await message.answer(ai_response)
             await state.set_state(Chat.active)
+        elif message.content_type == ContentType.PHOTO:
+            ... 
+            '''VISION MODEL'''
+        else: 
+            await message.answer('Нейросеть воспринимет только текстовые сообщения и изображения')
+
 
 @router.message(Chat.waiting)
 async def waiting(message: Message):
